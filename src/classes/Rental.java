07bf29588a516d1c7a3160c4724060ac8e1b8d95
johzip <johzip1010@gmail.com>
2020-05-27 16:34:40 +0200

    Outsource part of to big Funktion

--- src/classes/Rental.java
+++ src/classes/Rental.java
@@ -31,4 +31,13 @@ public class Rental {
 	    }
 	    return amountResult;
 	}
+	
+	public int getFrequentRenterPoints() {
+		// add frequent renter points
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+		    return 2;
+		return 1;
+	} 
+	
 }
